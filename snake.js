df00913156a136fbe33250593fc7813e3332d065
Shoikot Sen <shoikot@usf.edu>
2025-07-14 17:01:54 +0600

    Fix food alignment and eating logic for larger grid

--- snake.js
+++ snake.js
@@ -94,7 +94,7 @@ document.addEventListener("keydown", e => {
 });
 
 function getRandomFoodPosition() {
-    const x = Math.floor(Math.random() * 15) * 40;
-    const y = Math.floor(Math.random() * 15) * 40;
+    const x = Math.floor(Math.random() * 40) * 40;
+    const y = Math.floor(Math.random() * 40) * 40;
     return { x, y };
 }
